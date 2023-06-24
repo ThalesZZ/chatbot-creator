4edf9c4a6bb5251f7ccfe1ed4b065805270b5329
ThalesZZ <thazars@hotmail.com>
2023-06-24 13:25:33 -0300

    added buttons to sidebar

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,6 +3,8 @@
 import useChatbots from '@/services/api/hooks/useChatbots'
 import React from 'react'
 import styled from 'styled-components'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import { Link } from '@mui/material'
 
 export default function Home() {
   const [chatbots] = useChatbots()
@@ -10,12 +12,29 @@ export default function Home() {
   return (
     <main style={{ height: '100vh' }}>
       <Sidebar>
-        <h2>Chatbots</h2>
-        <Stack>
-          {chatbots.map((chatbot) => (
-            <Item key={chatbot.id}>{chatbot.name}</Item>
-          ))}
-        </Stack>
+        <div>
+          <h2>
+            chatbots <AddCircleIcon />
+          </h2>
+          <Stack>
+            {chatbots.map((chatbot) => (
+              <Item key={chatbot.id}>{chatbot.name}</Item>
+            ))}
+          </Stack>
+        </div>
+
+        <div id="credits">
+          feito por
+          <Link
+            color="inherit"
+            marginLeft={1}
+            href="https://github.com/ThalesZZ"
+            target="_blank"
+            sx={{ ':hover': { color: 'var(--blue-sapphire)' } }}
+          >
+            Thales Zarzar
+          </Link>
+        </div>
       </Sidebar>
     </main>
   )
@@ -23,14 +42,29 @@ export default function Home() {
 
 const Sidebar = styled.nav`
   flex-flow: column;
-  gap: 8px;
+  justify-content: space-between;
   background-color: var(--box);
   width: 12.5%;
-  min-height: 200px;
+  min-width: 230px;
+  gap: 8px;
   height: 100%;
 
-  > h2 {
-    padding: 12px;
+  > div:first-child {
+    flex-flow: column;
+
+    > h2 {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 12px;
+    }
+  }
+
+  > #credits {
+    padding: 8px;
+    background-color: var(--box-border);
+    border-top: 2px solid var(--bg-color);
+    font-size: 14px;
   }
 `
 
