dc0e044f7c8dff33833465058d5980e578fd2848
ThalesZZ <thazars@hotmail.com>
2023-06-24 00:50:01 -0300

    listing bots with stack

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,33 @@
 'use client'
 
 import useChatbots from '@/services/api/hooks/useChatbots'
+import { Paper, Stack } from '@mui/material'
 import React from 'react'
+import { styled } from '@mui/material/styles'
 
 export default function Home() {
   const [chatbots] = useChatbots()
 
-  return <main>{JSON.stringify(chatbots)}</main>
+  return (
+    <main>
+      <ChatbotSideList>
+        {chatbots.map((chatbot) => (
+          <Item key={chatbot.id}>{chatbot.name}</Item>
+        ))}
+      </ChatbotSideList>
+    </main>
+  )
 }
+
+const ChatbotSideList = styled(Stack)(() => ({
+  backgroundColor: '#00cc80',
+  gap: 8,
+  width: '12.5vw',
+}))
+
+const Item = styled(Paper)(() => ({
+  backgroundColor: 'transparent',
+  ':hover': {
+    backgroundColor: 'red',
+  },
+}))
