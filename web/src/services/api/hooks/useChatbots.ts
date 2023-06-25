d1c25328ff07d31316dc944283b57a28ebe5a401
ThalesZZ <thazars@hotmail.com>
2023-06-25 12:41:51 -0300

    delete webapp

--- web/src/services/api/hooks/useChatbots.ts
+++
@@ -1,7 +0,0 @@
-import { Chatbot } from '@/models/Chatbot'
-import API from '..'
-import useFetch, { UseFetch } from '@/services/hooks/useFetch'
-
-export default function useChatbots(): UseFetch<Chatbot[]> {
-  return useFetch([], API.chatbot.getAll)
-}
