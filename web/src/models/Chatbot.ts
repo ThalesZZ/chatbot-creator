db2964791414980c968932e624d7b58b5414cbeb
ThalesZZ <thazars@hotmail.com>
2023-06-25 11:48:20 -0300

    creadt chatflow model class on web

--- web/src/models/Chatbot.ts
+++ web/src/models/Chatbot.ts
@@ -1,39 +1,36 @@
+import { Chatflow, ChatflowDTO } from './Chatflow'
+
 export interface ChatbotDTO {
   id: string
   name: string
-  createdAt: string
-  updatedAt: string
+  flows: ChatflowDTO[]
 }
 
 export interface ChatbotProps {
   id: string
   name: string
-  createdAt: Date
-  updatedAt: Date
+  flows: Chatflow[]
 }
 
 export class Chatbot implements ChatbotProps {
   readonly id: string
   readonly name: string
-  readonly createdAt: Date
-  readonly updatedAt: Date
+  readonly flows: Chatflow[]
 
   constructor(props: ChatbotProps) {
     this.id = props.id
     this.name = props.name
-    this.createdAt = props.createdAt
-    this.updatedAt = props.updatedAt
+    this.flows = props.flows
   }
 
   static fromArrayDTO(array: ChatbotDTO[]): Chatbot[] {
-    return array.map(this.fromDTO)
+    return array?.map(this.fromDTO) || []
   }
 
   static fromDTO(dto: ChatbotDTO): Chatbot {
     return new Chatbot({
       ...dto,
-      createdAt: new Date(dto.createdAt),
-      updatedAt: new Date(dto.updatedAt),
+      flows: Chatflow.fromArrayDTO(dto.flows),
     })
   }
 }
